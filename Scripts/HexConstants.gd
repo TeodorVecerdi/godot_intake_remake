534d2b360539f9d05fe1f6a67e488e16524275e4
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-27 15:26:59 +0200

    Setup hex grid

--- Scripts/HexConstants.gd
+++ Scripts/HexConstants.gd
@@ -1,12 +1,14 @@
 class_name HexConstants
 
-const RADIUS: float = 100.0
-const INNER_CONSTANT: float = 0.8660254037
+const RADIUS: float = 101.0
+const INNER_CONSTANT: float = 0.891089109
 
-static func WorldToArray(x: float, y: float, radius: float) -> Vector2:
+static func WorldToArray(x: float, y: float, scale: float) -> Vector2:
+	var radius = scale * RADIUS
 	return Vector2(x / INNER_CONSTANT / radius / 2.0 - int(y) % 2 * 0.5, y / radius / 1.5)
 
-static func ArrayToWorld(x: float, y: float, radius: float) -> Vector2:
+static func ArrayToWorld(x: float, y: float, scale: float) -> Vector2:
+	var radius = scale * RADIUS
 	return Vector2((x + int(y) % 2 * 0.5) * INNER_CONSTANT * radius * 2.0, y * radius * 1.5)
 
 const NeighbourDelta = [[[0, 1], [1, 0], [0, -1], [-1, -1], [-1, 0], [-1, 1]],[[1, 1], [1, 0], [1, -1], [0, -1], [-1, 0], [0, 1]]]
--- Scripts/HexGrid.gd
+++ Scripts/HexGrid.gd
@@ -6,14 +6,31 @@ const HexCell = preload("res://Scenes/HexCell.tscn")
 
 export (int) var CellsX: int = 4
 export (int) var CellsY: int = 4
+export var Scale: float = 1.0
+export (Vector2) var Offset: Vector2 = Vector2(90, 101)
 
-onready var normalCells: HexCellTexture = $CellTiles
-onready var hiddenCells: HexCellTexture = $HiddenTiles
-onready var finishCells: HexCellTexture = $FinishTiles
+onready var normalCells: HexCellTexture = $Tiles/CellTiles
+onready var hiddenCells: HexCellTexture = $Tiles/HiddenTiles
+onready var finishCells: HexCellTexture = $Tiles/FinishTiles
+onready var cellContainer = $Cells
 
+var cellArray
 
 func _ready():
-	var hexCell = HexCell.instance()
-	hexCell.initialize(Vector2(100, 100), normalCells.getTextureRandom())
-	add_child(hexCell)
+
+	cellArray = []
+	for y in range(CellsY):
+		cellArray.append([])
+		for x in range(CellsX):
+			cellArray[y].append(HexCell.instance())
+			var texType = randi() % 2
+			if texType == 0:
+				cellArray[y][x].initialize(HexConstants.ArrayToWorld(x, y, Scale), normalCells.getTextureRandom())
+			else:
+				cellArray[y][x].initialize(HexConstants.ArrayToWorld(x, y, Scale), hiddenCells.getTextureRandom())
+			cellArray[y][x].scale = Vector2(Scale, Scale)
+			cellContainer.add_child(cellArray[y][x])
+			cellContainer.position = Offset * Scale
+
+	
 
