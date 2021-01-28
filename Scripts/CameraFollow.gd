171af509a5d14acc89de64e3cbc671c12f6707f8
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-28 15:07:04 +0200

    Add camera zoom state and signal emitting

--- Scripts/CameraFollow.gd
+++ Scripts/CameraFollow.gd
@@ -4,9 +4,12 @@ class_name CameraFollow
 export (float) var followSpeed: float = 0.1
 export (Vector2) var limitPadding: Vector2 = Vector2(350, 200)
 
+signal zoomChanged(state)
+
 onready var target = $"../Player"
 onready var grid: HexGrid = $".".get_parent() as HexGrid
 
+var currentZoomState = false
 
 func _ready():
 	var maxWidth = 2.0 * grid.CellsX * HexConstants.RADIUS * HexConstants.INNER_CONSTANT * grid.Scale
@@ -20,3 +23,13 @@ func _ready():
 
 func _process(_delta) -> void:
 	position = target.position
+
+
+func _input(event):
+    var justPressed = event.is_pressed() and not event.is_echo() 
+    if not justPressed:
+        return
+    
+    if Input.is_key_pressed(KEY_SPACE):
+        currentZoomState = !currentZoomState
+        emit_signal("zoomChanged", currentZoomState)
