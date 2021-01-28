e75540293a1262ffea0fee4faa504323c6798e29
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-28 17:04:57 +0200

    Setup color changing when reaching brackets

--- Scripts/LevelTimer.gd
+++ Scripts/LevelTimer.gd
@@ -17,36 +17,40 @@ var timeLeft: float
 var totalTime: float
 
 var enabled = false
+var fillAmount: float
 
 
 func _process(delta) -> void:
 	if not enabled:
 		return
-	
-	
+
 	timeLeft -= delta
 	if timeLeft <= 0:
 		timeLeft = 0
 		enabled = false
 		emit_signal("completed")
-	
-	var fillAmount = timeLeft / totalTime
-	var oldFillAmount = TimerProgress.ratio
-	# change good -> warning
-	if oldFillAmount > WarningBracket and fillAmount <= WarningBracket:
-		TimerProgress.modulate = WarningColor
-	elif oldFillAmount > BadBracket and fillAmount <= BadBracket:
-		TimerProgress.modulate = BadColor
 
+	var newFillAmount = timeLeft / totalTime
+
+	# change good -> warning
+	if fillAmount > WarningBracket and newFillAmount <= WarningBracket:
+		TimerProgress.tint_progress = WarningColor
+
+	# change warning -> bad
+	if fillAmount > BadBracket and newFillAmount <= BadBracket:
+		TimerProgress.tint_progress = BadColor
+
+	fillAmount = newFillAmount
 	TimerProgress.ratio = fillAmount
 	TimerText.text = "%.1fs / %.1fs" % [timeLeft, totalTime]
 
 
 func start(_totalTime: float, _timeLeft: float = -1.0):
-	TimerProgress.modulate = GoodColor
+	TimerProgress.tint_progress = GoodColor
 	totalTime = _totalTime
 	timeLeft = _timeLeft if _timeLeft != -1.0 else totalTime
 	enabled = true
+	fillAmount = 1.0
 
 
 func stop():
