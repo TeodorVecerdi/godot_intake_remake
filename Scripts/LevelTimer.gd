0be7399eb0cbb17de8200bfc73cc0d0fc9916754
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-28 16:32:48 +0200

    Add functions for starting/stopping timer and signals

--- Scripts/LevelTimer.gd
+++ Scripts/LevelTimer.gd
@@ -1,21 +1,39 @@
 extends Control
 class_name LevelTimer
 
-onready var TimerProgress = $TimerProgress
-onready var TimerText = $TimerText
+onready var TimerProgress = $Timer/TimerProgress
+onready var TimerText = $Timer/TimerText
+
+signal completed
+signal stopped
 
 var timeLeft: float
 var totalTime: float
 
-
-func _ready() -> void:
-	totalTime = 60.0
-	timeLeft = totalTime
+var enabled = false
 
 
 func _process(delta) -> void:
+	if not enabled:
+		return
+
 	timeLeft -= delta
+	if timeLeft <= 0:
+		timeLeft = 0
+		enabled = false
+		emit_signal("completed")
 
 	var fillAmount = timeLeft / totalTime
 	TimerProgress.ratio = fillAmount
 	TimerText.text = "%.1fs / %.1fs" % [timeLeft, totalTime]
+
+
+func start(_totalTime: float, _timeLeft: float = -1.0):
+	totalTime = _totalTime
+	timeLeft = _timeLeft if _timeLeft != -1.0 else totalTime
+	enabled = true
+
+
+func stop():
+	enabled = false
+	emit_signal("stopped")
