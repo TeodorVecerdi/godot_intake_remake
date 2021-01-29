041ccdbfe7f74b3cb2aa7679d25b93639cb8684a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-29 17:46:11 +0200

    Add level fade in/out animations to camera

--- Scripts/CameraFollow.gd
+++ Scripts/CameraFollow.gd
@@ -2,12 +2,16 @@ extends Camera2D
 class_name CameraFollow
 
 export (float) var followSpeed: float = 0.1
+export (float) var levelTransitionSpeed: float = 2
 export (Vector2) var normalZoom: Vector2 = Vector2(1, 1)
 export (Vector2) var limitPadding: Vector2 = Vector2(350, 200)
 
 signal zoomChanged(state)
+signal onLevelFadeOut
+signal onLevelFadeIn
 
 onready var target = $"../Player"
+onready var transitionFade = $"../../UI Canvas/Fade"
 onready var grid: HexGrid = $".".get_parent() as HexGrid
 
 var currentZoomState = false
@@ -18,8 +22,13 @@ var center: Vector2
 var gridWidth: float
 var gridHeight: float
 
+var tween: Tween
+
 
 func _ready() -> void:
+	tween = Tween.new()
+	add_child(tween)
+
 	gridWidth = 2.0 * grid.CellsX * HexConstants.RADIUS * HexConstants.INNER_CONSTANT * grid.Scale
 	gridHeight = 1.5 * grid.CellsY * HexConstants.RADIUS * grid.Scale
 	center = Vector2(gridWidth / 2.0, gridHeight / 2.0 - HexConstants.RADIUS * 1.2)
@@ -70,6 +79,27 @@ func setZoom(zoomState) -> void:
 		zoom = normalZoom
 		shouldFollowPlayer = true
 
+
+func levelFadeOut() -> void:
+	print("FADING OUT")
+	smoothing_enabled = false
+	tween.interpolate_property(self, "position", center, Vector2(center.x, center.y - 2000), levelTransitionSpeed, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.interpolate_property(transitionFade, "modulate", Color(0,0,0,0), Color(0,0,0,1), 0.5,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.2)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	print("FADING OUT COMPLETE")
+	emit_signal("onLevelFadeOut")
+	
+	
+func levelFadeIn() -> void:
+	print("FADING IN")
+	tween.interpolate_property(self, "position", Vector2(center.x, center.y + 2000), center, levelTransitionSpeed,Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.interpolate_property(transitionFade, "modulate", Color(0,0,0,1), Color(0,0,0,0), 0.5,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, levelTransitionSpeed-0.7)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	print("FADING IN COMPLETE")
+	smoothing_enabled = true
+	emit_signal("onLevelFadeIn")
 
 func _onHexGridLevelCompleted() -> void:
 	currentZoomState = true
