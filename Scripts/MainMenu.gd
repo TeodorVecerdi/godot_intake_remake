65ca38c3fe7e91d51462271414c07b62c92aafac
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-30 16:02:44 +0200

    Add main menu fade

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,12 +1,26 @@
 extends Control
 
 onready var playButton = $"CenterContainer/Menu Buttons/Play"
+onready var fade = $"Background Fade"
+
+var tween: Tween
 
 func _ready():
+	tween = Tween.new()
+	add_child(tween)
+	
 	playButton.grab_focus()
+
+	tween.interpolate_property(fade, "modulate", Color(0,0,0,1), Color(0,0,0,0), 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.5)
+	tween.start()
+	yield(tween, "tween_completed")
+
 
 
 func _onPlayPressed() -> void:
+	tween.interpolate_property(fade, "modulate", Color(0,0,0,0), Color(0,0,0,1), 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.5)
+	tween.start()
+	yield(tween, "tween_completed")
 	SceneManager.LoadScene(SceneManager.GAME)
 
 func _onTutorialPressed() -> void:
