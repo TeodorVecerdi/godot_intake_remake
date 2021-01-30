db3355e9805c574565a93a0e7ff0d080a5464035
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-30 16:56:37 +0200

    Animate player sprite change

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -78,17 +78,14 @@ func move(direction: int) -> void:
 	# position += HexConstants.DistanceToNeighbours[direction] * Grid.Scale * HexConstants.RADIUS
 	gridX += HexConstants.NeighbourDelta[gridY % 2][direction][0]
 	gridY += HexConstants.NeighbourDelta[gridY % 2][direction][1]
-
 	
+	var newPlayerTexture = getPlayerTexture(direction)
+	if newPlayerTexture != PlayerSprite.texture:
+		tweenPlayer.interpolate_property(PlayerSprite, "scale", Vector2(1,1), Vector2(0,0), 0.1, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+		tweenPlayer.interpolate_callback(self, 0.1, "setPlayerTexture", newPlayerTexture)
+		tweenPlayer.interpolate_property(PlayerSprite, "scale", Vector2(0,0), Vector2(1,1), 0.1, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.1)
+
 	tweenPlayer.start()
-	if direction == 2 or direction == 3:  # front sprite
-		PlayerSprite.texture = Front
-	elif direction == 0 or direction == 5:  # back sprite
-		PlayerSprite.texture = Back
-	elif direction == 4:  # left sprite
-		PlayerSprite.texture = Left
-	else:  # right sprite
-		PlayerSprite.texture = Right
 
 	updateValidMoves()
 	updateArrows()
@@ -125,3 +122,18 @@ func updateArrows() -> void:
 	for i in range(6):
 		Arrows[i].visible = validMoves[i]
 	emit_signal("arrowsDone")
+
+
+func getPlayerTexture(direction: int) -> Texture:
+	if direction == 2 or direction == 3:  # front sprite
+		return Front
+	elif direction == 0 or direction == 5:  # back sprite
+		return Back
+	elif direction == 4:  # left sprite
+		return Left
+	else:  # right sprite
+		return Right
+
+
+func setPlayerTexture(texture: Texture) -> void:
+	PlayerSprite.texture = texture
