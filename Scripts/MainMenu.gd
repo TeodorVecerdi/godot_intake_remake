aa0e1a716e404373c94c9e10a642285a22b78642
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-01-31 16:59:00 +0200

    Change main menu to load game settings scene

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -21,7 +21,7 @@ func _onPlayPressed() -> void:
 	tween.interpolate_property(fade, "modulate", Color(0,0,0,0), Color(0,0,0,1), 1.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT, 0.3)
 	tween.start()
 	yield(tween, "tween_completed")
-	SceneManager.LoadScene(SceneManager.GAME)
+	SceneManager.LoadScene(SceneManager.GAME_SETTINGS)
 
 func _onTutorialPressed() -> void:
 	SceneManager.LoadScene(SceneManager.TUTORIAL)
